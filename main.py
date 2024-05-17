d5de39353f38c8469f9a8a540b7ba2509beb826e
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-17 14:11:53 +0530

    use dthe function update-todo

--- main.py
+++ main.py
@@ -47,10 +47,12 @@ async def post_todo(todo: Todo):
         return response
     raise HTTPException(400, "Something went wrong")
 
-@app.put("/api/todo{id}")
-async def put_todo(id,data):
-    return 1
-
+@app.put("/api/todo/{title}/", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo with the title {title}")
 
 @app.delete("/api/todo{id}")
 async def delete_todo(id):

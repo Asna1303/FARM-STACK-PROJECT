d9609ad4ee9b303601d367d6efdeb4ef8eb9f5da
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-17 10:19:05 +0530

    cretaed a function call fetch_all_todos to get all todos

--- database.py
+++ database.py
@@ -11,3 +11,10 @@ collection = database.todo
 async def fetch_one_todo(title):
     document = await collection.find_one({"title": title})
     return document
+
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+    async for document in cursor:
+        todos.append(Todo(**document))
+    return todos

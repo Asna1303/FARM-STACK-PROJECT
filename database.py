743a507334078c9a549b2ba50223843079456aa4
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-17 10:14:01 +0530

    Add fetch_one_todo async function to retrieve a todo item by title

--- database.py
+++ database.py
@@ -7,3 +7,7 @@ import motor.motor_asyncio
 client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
 database = client.TodoList
 collection = database.todo
+
+async def fetch_one_todo(title):
+    document = await collection.find_one({"title": title})
+    return document
